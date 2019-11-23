3ab9526d0d65a7aa59948fa9a7c87a8732d36c72
Matt Storey <storeyms2010@gmail.com>
2019-11-22 18:04:11 -0600

    set1 complete

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengeSets
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             if (num1 == num2)
-	        {
+            {
                 return true;
             }
             return false;
@@ -14,32 +14,60 @@ namespace ChallengeSets
 
         public double Subtract(double minuend, double subtrahend)
         {
-            (minuend - subtrahend);
+            double sum = minuend - subtrahend;
+
+            return sum;
+
+
         }
 
         public int Add(int number1, int number2)
         {
-            (number1 + number2);
+            int sum = number1 + number2;
+
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+
+            else
+            {
+
+                return number1;
+            }
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            (factor1 * factor2);
+            long sum = factor1 * factor2;
+
+            return sum;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+
+
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+
+            }
+
+
+            return $"Hello, {nameOfPerson}!";
+
+
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
