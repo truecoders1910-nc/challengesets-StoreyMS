5f99b6a0e03d106bacc3c17105ba5534f013a113
Matt Storey <storeyms2010@gmail.com>
2020-02-14 18:19:24 -0600

    set 7 complete

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -1,41 +1,67 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
 namespace ChallengeSets
 {
     public class ChallengeSet01
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            throw new NotImplementedException();
+           if(num1 == num2)
+            {
+                return true;
+            }
+           else return false;
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+            double diff = minuend - subtrahend;
+            return diff;
+           
         }
+
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            int sum = number1 + number2;
+            return sum;
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            if (number1 > number2)
+            {
+                return number2;
+            }
+            else return number1;
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            long num = factor1 * factor2;
+            return num;
         }
 
         public string GetGreeting(string nameOfPerson)
         {
-            throw new NotImplementedException();
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+
+
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
         }
 
         public string GetHey()
         {
-            throw new NotImplementedException();
+            return "HEY!";
         }
     }
 }
